0141841603b0c9174901bd7a81cfc6577a9cd754
vamsi krishna <krishna@maguresoftwares.com>
2019-04-24 14:56:20 +0530

    added routing

---
+++ src/components/Error/ErrorComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ErrorComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>404</h1>Error occured while navigating ...!
+      </div>
+        )
+    }
+}
+
+export default ErrorComponent
