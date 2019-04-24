0450ebd81ba6a1e7753dc8a1449e3ddae46ccf3e
vamsi krishna <krishna@maguresoftwares.com>
2019-04-24 15:24:56 +0530

    with HttpGetService

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TodolistComponent from './components/todolist/index';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ErrorComponent from './components/Error/ErrorComponent';
+import HttpComponent from './components/HttpService/HttpComponent';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={TodolistComponent} exact />
+          <Route path="/httpservice" component={HttpComponent} />
           <Route component={ErrorComponent} />
         </Switch>
       </BrowserRouter>
