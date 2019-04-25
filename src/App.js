0119fbc0ab3c1469ce582829a1c40dd41c78cb71
vamsi krishna <krishna@maguresoftwares.com>
2019-04-25 10:43:17 +0530

    with redux.js file

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ErrorComponent from './components/Error/ErrorComponent';
 import HttpComponent from './components/HttpService/HttpComponent';
 import Registration from './components/Registration/Registration';
+import redux from './components/redux/redux';
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <TodolistComponent />
-      // </div>
       <BrowserRouter>
         <Switch>
           <Route path="/" component={TodolistComponent} exact />
           <Route path="/httpservice" component={HttpComponent} />
           <Route path="/registration" component={Registration} />
+          <Route path="/redux" component={redux} />
           <Route component={ErrorComponent} />
         </Switch>
       </BrowserRouter>
---
+++ src/components/redux/redux.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class redux extends Component {
+    render() {
+        return (
+            <div>
+                <h1>return the redux store</h1>
+            </div>
+        )
+    }
+}
+
+export default redux
